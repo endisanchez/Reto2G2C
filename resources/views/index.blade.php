adc0e2b6b4d00712a5bd0b258d78ad4a5574c8e0
DW32 <ik012982i10@H012982.NET>
2020-10-26 12:59:05 +0100

    primer codigo

---
+++ resources/views/index.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="eu">
+<head>
+<link rel="stylesheet" type="text/css" href="../resources/css/estilos.css">
+
+<meta charset="utf-8"/>
+
+<title>EuskoDenda</title>
+</head>
+
+
+<body id="body">
+    <nav id="menu">
+          <ul>
+              <li><a href="index.php">HOME</a></li>
+              <li><a href="productos.php">PRODUCTOS</a></li>
+              <li><a href="contacto.php">CONTACTO</a></li></li>
+              <li><a href="admin.php">ADMIN</a></li>
+          </ul>
+     </nav>
+
+<div id="logoprincipal">
+  <img src="./Imagenes/logo.png" alt="logo" />
+</div>
+
+<article>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+  @foreach($productos as $item)
+    <tr>
+      <th scope="row">{{$item->id}}</th>
+      <td>{{$item->nombre}}</td>
+      <td>{{$item->descripcion}}</td>
+      <td>@mdo</td>
+    </tr>
+  @endforeach()
+  </tbody>
+</table>
+
+</article>
+
+<footer>
+  <a href="http://www.facebook.com"><img src="./Imagenes/logofacebook.png" alt="logo" width="45px" height="45px" /><label style="color:white">Facebook</label><a>
+  <a href="http://www.instagram.com"><img src="./Imagenes/logoinstagram.png" alt="logo" width="45px" height="45px" /><label style="color:white">Instagram</label><a>
+  <a href="http://www.twitter.com"><img src="./Imagenes/logotwitter.png" alt="logo" width="45px" height="45px" /><label style="color:white">Twitter</label><a>
+</footer>
+
+</body>
+
+</html>
