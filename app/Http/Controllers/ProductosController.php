2c7c10483972f4c23bbb9d01f315eecd65bdb5d9
Miguelzp <mzabalpe19dw31@ikzubirimanteo.com>
2020-10-28 11:46:42 +0100

    Productos vista y controller (sin menu desplegable)

--- app/Http/Controllers/ProductosController.php
+++ app/Http/Controllers/ProductosController.php
@@ -8,7 +8,16 @@ class ProductosController extends Controller
 {
     public function inicio()
     {
-        $productos = App\Models\Productos::all();
-        return view('index', compact('productos'));
+        $productos = App\Models\productos::all();
+        $tiendas = App\Models\tiendas::all();
+        return view('productos', compact('productos','tiendas'));
     }
+
+    //SELECT * FROM productos INNER JOIN tiendas_has_productos ON productos.id = tiendas_has_productos.productos_id WHERE tiendas_id = 1;
+
+    static function productosPorTienda($idtienda) {
+      $productosTienda = App\Models\productos::select("SELECT * FROM productos INNER JOIN tiendas_has_productos ON productos.id = tiendas_has_productos.productos_id WHERE tiendas_id = $idtienda;");
+      return view('productos', compact('productosTienda'));
+    }
+
 }
