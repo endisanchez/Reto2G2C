07d2e863bd36a3720c08b7eba30db540eb14a1ec
Miguelzp <mzabalpe19dw31@ikzubirimanteo.com>
2020-10-30 08:11:30 +0100

    Rutas de edición de productos añadidas

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/admin/{id}', 'App\Http\Controllers\prodeditController@edit')->name('gestion');
+Route::get('/admin/{id}', 'App\Http\Controllers\ProductosController@edit')->name('gestion');
+Route::put('/admin/{id}', 'App\Http\Controllers\ProductosController@updateProd')->name('update');
 
-Route::get('admin', 'App\Http\Controllers\adminController@admin');
+Route::get('admin', 'App\Http\Controllers\ProductosController@admin');
 
 Route::get('productos', 'App\Http\Controllers\ProductosController@inicio');
+Route::post('productos', 'App\Http\Controllers\ProductosController@productosPorTienda')->name('prodTiendas');
 
+Route::post('index', 'App\Http\Controllers\ProductosController@crear')->name('crearprod');
+
+Route::delete('/productos/{id}', 'App\Http\Controllers\ProductosController@eliminar')->name('eliminarProd');
+
+Route::get('contacto', function () {
+    return view('contacto');
+});
+
+Route::get('/', function () {
+    return view('index');
+});
 
 Route::get('/index', function () {
     return view('index');
